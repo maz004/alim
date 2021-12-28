005a5b33f2e4ddb8f28a6d31fdcd9a479a6b0fc7
unknown <ali.messoudi99@gmail.com>
2021-12-27 23:29:11 -0600

    make it better

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Users;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("blocs")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    
+    @PostMapping("/save")
+    public void save(@RequestBody Users users)
+    {userRepository.save(users);}
+
+
+
+
+}
