282d7e8108772ba56962e5aab9b67b9e79a09420
unknown <ali.messoudi99@gmail.com>
2022-01-13 12:27:57 -0600

    make it better

--- src/main/java/com/example/demo/controller/CrSalleController.java
+++ src/main/java/com/example/demo/controller/CrSalleController.java
@@ -22,98 +22,106 @@ import java.util.List;
 
 public class CrSalleController {
 
-	@Autowired
-	private CrSalleRepository crSalleRepository;
+    @Autowired
+    private CrSalleRepository crSalleRepository;
 
-	@Autowired
-	private UserRepository u;
+    @Autowired
+    private UserRepository u;
 
-	@Autowired
-	private GetUserWithHTTPServletRequestController oui;
+    @Autowired
+    private GetUserWithHTTPServletRequestController oui;
 
-	@GetMapping("/all")
-	public List<CrSalle> findAll() {
+    @GetMapping("/all")
+    public List<CrSalle> findAll() {
 
-		return crSalleRepository.findAll();
-	}
+        return crSalleRepository.findAll();
+    }
 
-	@PostMapping("/save")
-	public void save(@RequestBody CrSalle crs) {
-		crSalleRepository.save(new CrSalle(new Key(crs.getSalle().getId(),crs.getCrenaux().getId()),crs.getDate(),crs.getSalle(),crs.getCrenaux()));
-	}
+    @PostMapping("/save")
+    public void save(@RequestBody CrSalle crs) {
+        crSalleRepository.save(new CrSalle(new Key(crs.getSalle().getId(), crs.getCrenaux().getId()), crs.getDate(), crs.getSalle(), crs.getCrenaux()));
+    }
 
-	@DeleteMapping(value = "/delete/{id}")
-	public void delete(@PathVariable(required = true) String id) {
-		System.out.println("id = "+id);
-		CrSalle crs = crSalleRepository.findById(Long.parseLong(id));
-		crSalleRepository.delete(crs);
-	}
+    @DeleteMapping(value = "/delete/{id}")
+    public void delete(@PathVariable(required = true) String id) {
+        System.out.println("id = " + id);
+        CrSalle crs = crSalleRepository.findById(Long.parseLong(id));
+        crSalleRepository.delete(crs);
+    }
 
-	@GetMapping(value = "/count")
-	public long count() {
+    @GetMapping(value = "/count")
+    public long count() {
 
-		return crSalleRepository.count();
-	}
+        return crSalleRepository.count();
+    }
 
-	@GetMapping("/alll")
-	public CrSalle findByIdd(String salle, String crenaux) {
-		List<CrSalle> lst = crSalleRepository.findAll();
-		CrSalle crs2 = null;
-		for (CrSalle crs : lst) {
-			if (crs.getSalle().getId() == Integer.parseInt(salle) && crs.getCrenaux().getId() == Integer.parseInt(crenaux)) {
-				crs2 = crs;
-			}
-		}
-		return crs2;
-	}
+    @GetMapping("/alll")
+    public CrSalle findByIdd(String salle, String crenaux) {
+        List<CrSalle> lst = crSalleRepository.findAll();
+        CrSalle crs2 = null;
+        for (CrSalle crs : lst) {
+            if (crs.getSalle().getId() == Integer.parseInt(salle) && crs.getCrenaux().getId() == Integer.parseInt(crenaux)) {
+                crs2 = crs;
+            }
+        }
+        return crs2;
+    }
 
-	@DeleteMapping(value = "/deletee/{salle,crenaux}")
-	public void delete( String salle, String creneaux) {
-		CrSalle crs = findByIdd(salle,creneaux);
-		String admin = "admin";
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String username = auth.getName();
-		if(username.equals(admin)){
-		crSalleRepository.delete(crs);}
-	}
+    @DeleteMapping(value = "/deletee/{salle,crenaux}")
+    public void delete(String salle, String creneaux) {
+        CrSalle crs = findByIdd(salle, creneaux);
+        String admin = "admin";
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        String rej = "rejette";
+        String encr = "En cour de traitement";
+        String user = "user";
+        String pp = "Non valide";
+        if (username.equals(admin)) {
+            crSalleRepository.delete(crs);
+        } else if (username.equals(user) && (crs.getStatus().equals(encr) || crs.getStatus().equals(pp))){
+            crSalleRepository.delete(crs);
+        }
+    }
 
-	@RequestMapping(value = "/myusername", method = RequestMethod.GET)
-	@ResponseBody
-	public String currentUserName(Principal principal) {
-		return principal.getName();
-	}
+    @RequestMapping(value = "/myusername", method = RequestMethod.GET)
+    @ResponseBody
+    public String currentUserName(Principal principal) {
+        return principal.getName();
+    }
 
-	@PostMapping(value = "/findme/{salle,crenaux}")
-	public void valider(String salle, String creneaux){
-		String admin = "admin";
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String username = auth.getName();
-		CrSalle crs = findByIdd(salle,creneaux);
-		String valide = "valide";
-		String user = "user";
-		String reject = "rejette";
-		String trt = "En cour de traitement";
+    @PostMapping(value = "/findme/{salle,crenaux}")
+    public void valider(String salle, String creneaux) {
+        String admin = "admin";
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        CrSalle crs = findByIdd(salle, creneaux);
+        String valide = "valide";
+        String user = "user";
+        String reject = "rejette";
+        String trt = "En cour de traitement";
 
-		if( username.equals(admin)  ){
-		crs.setStatus("valide");
-		crSalleRepository.save(crs);
-		}else if(username.equals(user) && (crs.getStatus().equals(reject) || crs.getStatus().equals(trt))){
-			crs.setStatus("En cour de traitement");
-			crSalleRepository.save(crs);
-		}
+        if (username.equals(admin)) {
+            crs.setStatus("valide");
+            crSalleRepository.save(crs);
+        } else if (username.equals(user) && (crs.getStatus().equals(reject) || crs.getStatus().equals(trt))) {
+            crs.setStatus("En cour de traitement");
+            crSalleRepository.save(crs);
+        }
 
-}
-	@PostMapping(value = "/reject/{salle,crenaux}")
-	public void reject(String salle, String creneaux){
-		String admin = "admin";
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String username = auth.getName();
-		CrSalle crs = findByIdd(salle,creneaux);
+    }
 
-		if( username.equals(admin)){
-			crs.setStatus("rejette");
-			crSalleRepository.save(crs);
-		}
+    @PostMapping(value = "/reject/{salle,crenaux}")
+    public void reject(String salle, String creneaux) {
+        String admin = "admin";
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        CrSalle crs = findByIdd(salle, creneaux);
 
-	}
+        if (username.equals(admin)) {
+            crs.setStatus("rejette");
+            crSalleRepository.save(crs);
+        }
+
+    }
 }
